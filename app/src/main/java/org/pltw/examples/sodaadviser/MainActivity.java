699c2b847f74a3e02bc63aa1c123e6102145fc8b
bjhunt7520 <20hunbra@student.mcpsmt.org>
2017-11-27 10:34:40 -0700

    class work

--- app/src/main/java/org/pltw/examples/sodaadviser/MainActivity.java
+++ app/src/main/java/org/pltw/examples/sodaadviser/MainActivity.java
@@ -1,13 +1,27 @@
 package org.pltw.examples.sodaadviser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button find_soda;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        find_soda = (Button)findViewById(R.id.find_soda);
+
+        find_soda.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
